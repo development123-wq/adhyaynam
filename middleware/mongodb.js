99338851ac473629bd88ec57035395b33949a785
development123-wq <development@techzenon.com>
2025-06-27 18:44:29 +0530

    mongodb.js

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = "mongodb://127.0.0.1:27017/adhyayanamacademy";
+const MONGODB_URI = "mongodb+srv://development:Akash@123@cluster0.1hoarf8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 //const MONGODB_URI = "mongodb+srv://hitesh:QWdPRZ2LiXHVx8aD@cluster0.h16vtk6.mongodb.net/?retryWrites=true&w=majority"; //this is project0 id and password (for test)
 
 const connectDb = async () => {
